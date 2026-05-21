dac5ee5112b583575eac24233456c20554652c7a
ARBORso <chakrahari.anirudh@gmail.com>
2026-05-21 11:03:41 +0530

    Update page.tsx

--- app/session/[id]/page.tsx
+++ app/session/[id]/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useEffect, useState, useRef } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import { useParams, useRouter } from 'next/navigation'
 import { supabase, Move, MoveType, Session } from '@/lib/supabase'
 
